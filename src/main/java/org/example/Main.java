5101aa1a8bebe884d658c29143d8230b41edcf37
Petro <petrotymchuck@gmail.com>
2025-11-07 15:09:08 +0200

    add smtp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,24 @@
 package org.example;
 
+import org.example.data.smtp.EmailMessage;
+import org.example.services.SmtpService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
+
+//        SmtpService smtpService = new SmtpService();
+//
+//        EmailMessage email = new EmailMessage(
+//                "Тестовий лист",
+//                "<h2>Привіт, це тест!</h2><p>Працює Java SMTP 👌</p>",
+//                "tymchuksasho724@gmail.com"
+//        );
+//
+//        smtpService.sendEmail(email);
     }
 }
---
+++ src/main/java/org/example/data/smtp/EmailMessage.java
@@ -0,0 +1,24 @@
+package org.example.data.smtp;
+
+public class EmailMessage {
+    private String subject;
+    private String body;
+    private String to;
+
+    public EmailMessage() {}
+
+    public EmailMessage(String subject, String body, String to) {
+        this.subject = subject;
+        this.body = body;
+        this.to = to;
+    }
+
+    public String getSubject() { return subject; }
+    public void setSubject(String subject) { this.subject = subject; }
+
+    public String getBody() { return body; }
+    public void setBody(String body) { this.body = body; }
+
+    public String getTo() { return to; }
+    public void setTo(String to) { this.to = to; }
+}
